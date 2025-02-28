1c180fc193a48253899f5749b5f74c03f5047453
bio9900 <xcgffgh7@gmail.com>
2025-03-01 02:27:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
-from flask import Flask
+import aiohttp
+from flask import Flask, jsonify, request
+
 
 app = Flask(__name__)
+
+
+@app.route("/<path:path>", methods=["GET", "POST", "PUT",
+"DELETE"])
+async def main(path):
+    if request.get_data():
+        payload = request.get_json()
+    else:
+        payload = None
+
+    headers = dict(request.headers)
+    headers.pop("Host", None)
+    headers.pop("Content-Length", None)
+
+    async with aiohttp.request(
+        request.method,
+        f"https://gw.sandboxol.com/{path}",
+        headers=headers,
+        json=payload,
+        ssl=False
+    ) as response:
+        return jsonify(await response.json())
+
+
+app.run()
